9dd0d52b0e53d74597bbb9aa0dd805b4d56d8b54
Your Name <you@example.com>
2020-05-13 21:36:35 +0300

    no comement

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -72,7 +72,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(394, 417);
+            this.ClientSize = new System.Drawing.Size(454, 461);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
