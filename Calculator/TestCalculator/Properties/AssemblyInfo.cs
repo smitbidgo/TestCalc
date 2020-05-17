ac7b81a3636f9101f80f935c41751db0ca8aa913
vasul <dsds@ddd.ua>
2020-05-17 19:38:48 +0300

    Add test

---
+++ Calculator/TestCalculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestCalculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestCalculator")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eb16384e-ab4a-427b-8532-0be271476ace")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Calculator/TestCalculator/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcClas;
+using AnalaizerClass;
+namespace TestCalculator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Format_DoubleOperator_Error04Returned()
+        {
+            Analaizer.expression = "(10++2)";
+            Assert.AreEqual(Analaizer.expression.Substring(0, "Error 04".Length), "Error 04");
+        }
+       
+        
+       
+    }
+}
